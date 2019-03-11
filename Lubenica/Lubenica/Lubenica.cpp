a2c7c9fedfe80cc49cb78d1d4f7de95eafe2f96b
Sophia1307 <Sophia1307@users.noreply.github.com>
2019-03-11 21:37:58 +0100

    dodana vježba lubenica
    potrebno ispraviti

---
+++ Lubenica/Lubenica/Lubenica.cpp
@@ -0,0 +1,25 @@
+﻿// Lubenica.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int w;
+	cin >> w;
+	 
+	if (w % 2 == 0) {
+		(w / 2) % 2 == 0;
+		cout << "DA";
+
+	} else {
+
+		cout << "NE";
+	}
+		
+	return 0;
+}
---
+++ sreca/sreca/sreca.cpp
@@ -0,0 +1,29 @@
+﻿// sreca.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 
+
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int N, i, br_bombona, br = 0; // br je brojač koji broji sretne učenike
+
+	cin >> N;
+
+	for (i = 1; i <= N; i++) {
+
+		cin >> br_bombona;
+
+		if (br_bombona >= i)
+		{
+			br++;					//brojač sretnih učenika
+		}
+
+	}
+	cout << br;
+
+	return 0;
+}
